adccf8bf3de684212944a60e07abf697f0a91fdf
Franstio <franstiodiego01@gmail.com>
2024-07-15 09:17:01 +0700

    ok

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -95,7 +95,7 @@ const Home = () => {
         });
     }, []);
     useEffect(() => {
-        const weight = Scales50Kg?.weight50Kg ?? 0;
+        let weight = Scales50Kg?.weight50Kg ?? 0;
         const binWeight = container?.weightbin ?? 0;
 	    weight = weight - binWeight;
         //	console.log({w:weight,bin:binWeight,w2:Scales50Kg,c:container});
