d046098a8ad06cf9e109ef7ca13e2762536546aa
FahriEfendi <fahriefendi0795@gmail.com>
2024-09-24 15:24:33 +0700

    ok

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -549,11 +549,7 @@ const Home = () => {
                                 type="text"
                                 onChange={e => setScanData(e.target.value)}
                                 value={scanData}
-                                ref={inputRef}
-                                onBlur={()=>{
-                                    if (inputRef && inputRef.current)
-                                        inputRef.current.focus();                                    
-                                }}
+                                
                                 name="text"
                                 onKeyDown={e => handleKeyPress(e)}
                                 className="block w-full rounded-md border-0 py-2 px-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
